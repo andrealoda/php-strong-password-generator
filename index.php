e9c812ca37c314d24893e505154c3c3fe6ab7e9a
andrea8980 <andrea@casaloda.it>
2026-07-29 18:32:34 +0200

    Enhance form styling and input validation for password length

--- index.php
+++ index.php
@@ -5,6 +5,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        .container {
+            width: 500px;
+            margin: 0 auto;
+            text-align: center;
+            color: white;
+            background-color: #491d6d;
+            padding: 50px;
+            margin-top: 50px;
+        }
+
+        .title {
+            margin-bottom: 20px;
+        }
+
+        .subtitle {
+            margin-bottom: 20px;
+        }
+
+        .form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: #6a1b9a;
+            padding: 20px;
+        }
+
+        .label {
+            margin-bottom: 10px;
+        }
+
+        .input {
+            margin-bottom: 20px;
+            padding: 10px;
+            width: 200px;
+        }
+
+        .button {
+            padding: 10px 20px;
+            background-color: #f0a500;
+            border: none;
+            color: white;
+            cursor: pointer;
+        }
+
+        .error {
+            color: red;
+        }
+
+        .password {
+            font-weight: bold;
+            margin-top: 30px;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -12,9 +68,9 @@
     <div class="container">
         <h1 class="title">Strong Password Generator</h1>
         <h2 class="subtitle">Genera una password sicura</h2>
-        <form action="" method="GET">
+        <form class="form" action="" method="GET">
             <label for="length" class="label">Lunghezza password</label>
-            <input type="number" name="length" id="length" class="input" placeholder="inserisci la lunghezza della password">
+            <input type="number" name="length" id="length" class="input" placeholder="quanti caratteri vuoi?" min="1" required>
             <button type="submit" class="button">Genera</button>
         </form>
 
@@ -44,7 +100,7 @@
         }
 
         $password = passwordGenerator($length);
-        echo "<p class='password'>La tua password generata è: $password</p>";
+        echo "<h3 class='password'>La tua password generata è: </h3>" . "<br>" . $password;
 
         ?>
 
