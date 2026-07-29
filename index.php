5768b58564dcccf3dafb5d009d38d2e37f663b5c
andrea8980 <andrea@casaloda.it>
2026-07-29 17:48:36 +0200

    Add initial implementation of strong password generator form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+    <h1 class="title">Strong Password Generator</h1>
+    <h2 class="subtitle">Genera una password sicura</h2>
+    <form action="" method="GET">
+        <label for="length" class="label">Lunghezza password</label>
+        <input type="number" name="length" id="length" class="input" placeholder="inserisci la lunghezza della password">
+        <button type="submit" class="button">Genera</button>
+    </form>
+
+</div>
+
+</body>
+</html>
