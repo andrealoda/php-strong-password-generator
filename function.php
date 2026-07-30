fd8e786126eaea716f1db1b532d0387c0ea37bd4
andrea8980 <andrea@casaloda.it>
2026-07-30 12:09:55 +0200

    Refactor passwordGenerator function to improve character selection logic and ensure return type consistency

--- function.php
+++ function.php
@@ -2,26 +2,34 @@
 
 
 
-function passwordGenerator(int $length)
+function passwordGenerator(int $length): string
 {
     $numbers = '0123456789';
     $letters = 'abcdefghijklmnopqrstuvwxyz';
     $capitalLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $symbols = '!@#$%^&*()_+-=';
-    $characters = $numbers . $letters . $capitalLetter . $symbols;
-    $password = '';
 
     if ($length < 1 || !is_numeric($length) || $length > 20) {
         echo "<p class='error'>Inserisci un numero maggiore di 0 e minore o uguale a 20</p>";
-    } else {
-
-        for ($i = 0; $i < $length; $i++) {
-            $password .= $characters[random_int(0, strlen($characters) - 1)];
-        }
-        return $password;
+        return '';
     }
+
+    $characters = $numbers;
+
+    if (isset($_GET['include_letters']) && $_GET['include_letters'] === 'on') {
+        $characters .= $letters;
+    }
+
+    if (isset($_GET['include_capital_letters']) && $_GET['include_capital_letters'] === 'on') {
+        $characters .= $capitalLetter;
+    }
+    if (isset($_GET['include_symbols']) && $_GET['include_symbols'] === 'on') {
+        $characters .= $symbols;
+    }
+
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[random_int(0, strlen($characters) - 1)];
+    }
+    return $password;
 }
-
-
-
-?>
